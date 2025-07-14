c8575add325613a8f2b8e96e9ae9172726be80cc
Franco Galluccio <francoivangalluccio@gmail.com>
2025-07-14 08:48:25 -0300

    UPDATE CartSheet falsy condition

--- src/features/cart/components/CartSheet.tsx
+++ src/features/cart/components/CartSheet.tsx
@@ -99,7 +99,7 @@ export function CartSheet() {
           </ScrollArea>
         )}
 
-        {items.length && (
+        {items.length > 0 && (
           <SheetFooter className="mt-6 flex flex-col gap-2">
             <div className="flex justify-between text-sm font-medium">
               <span>Tipo de carrito:</span>
